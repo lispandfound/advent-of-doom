b7e316b2f55e869af6d9b6807eea7c0657c633e5
Jake Faulkner <jake@devbox.phoenix>
2024-02-25 19:54:10 +1300

    Solve Day 8

--- src/Days/Day08.hs
+++ src/Days/Day08.hs
@@ -10,10 +10,11 @@ import qualified Data.Set as Set
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
 import qualified Util.Util as U
-
+import Control.Applicative
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
 import Data.Void
+import Control.Monad.State
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +22,75 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = instruction `sepBy` endOfLine
+  where instruction = nop <|> acc <|> jmp
+        nop = "nop " *> pure Nop <*> signed decimal
+        acc = "acc " *> pure Acc <*> signed decimal
+        jmp = "jmp " *> pure Jmp <*> signed decimal
+
+data Instruction = Nop Int | Acc Int | Jmp Int deriving (Show, Eq)
+data ProgramState = ProgramState {
+  pc :: Int
+  , accumulator :: Int
+  , executed :: Set Int
+                                 } deriving (Show, Eq)
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Instruction]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Maybe Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA program = evalState run $ initialState
+  where
+    initialState = ProgramState 0 0 mempty
+    run = do
+          curPc <- pc <$> get
+          curAccumulator <- accumulator <$> get
+          exec <- executed <$> get
+          if curPc `Set.member` exec then
+            return curAccumulator
+          else do
+            case program !! curPc of
+              Nop _ -> modify (\st -> st {pc = curPc + 1})
+              Acc dx -> modify (\st -> st {pc = curPc + 1, accumulator = curAccumulator + dx})
+              Jmp j -> modify (\st -> st {pc = curPc + j})
+            modify (\st -> st {executed = Set.insert curPc exec})
+            run
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB program = join . find isJust . map (\i -> evalStateT (run i) initialState) . findIndices jmpOrNop $ program
+  where
+    jmpOrNop (Acc _) = False
+    jmpOrNop _ = True
+    initialState = ProgramState 0 0 mempty
+    run sw = do
+          curPc <- pc <$> get
+          curAccumulator <- accumulator <$> get
+          exec <- executed <$> get
+          guard $ not (curPc `Set.member` exec)
+          if curPc >= length program then
+            return curAccumulator
+          else do
+            case program !! curPc of
+              Nop i | curPc == sw -> jmp i
+              Nop _ -> nop
+              Acc dx -> acc dx
+              Jmp _ | curPc == sw -> nop
+              Jmp j -> jmp j
+            modify (\st -> st {executed = Set.insert curPc exec})
+            run sw
+    nop = do
+           curPc <- pc <$> get
+           modify (\st -> st {pc = curPc + 1})
+    acc dx = do
+           curPc <- pc <$> get
+           curAccumulator <- accumulator <$> get
+           modify (\st -> st {pc = curPc + 1, accumulator = curAccumulator + dx})
+    jmp j = do
+           curPc <- pc <$> get
+           modify (\st -> st {pc = curPc + j})
