f74a9262d7e47a8a9096fa8702cb3629287ac460
Jake Faulkner <jake@devbox.phoenix>
2024-02-26 10:20:24 +1300

    Solve Day 11

--- src/Days/Day11.hs
+++ src/Days/Day11.hs
@@ -1,7 +1,7 @@
 module Days.Day11 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
+import Data.List as List
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import Data.Maybe
@@ -10,7 +10,8 @@ import qualified Data.Set as Set
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
 import qualified Util.Util as U
-
+import qualified Util.Coordinates as Uc
+import qualified Util.Parsers as Up
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
 import Data.Void
@@ -21,19 +22,60 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser =  Up.coordinateParser (\case
+                                      'L' -> Just Empty
+                                      '#' -> Just Occupied
+                                      '.' -> Just Floor) 0
+
+
+
+data Cell = Empty | Floor | Occupied deriving (Show, Eq)
 
 ------------ TYPES ------------
-type Input = Void
+type Input = Uc.CoordinateMap Cell
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = U.count ((== Occupied) . snd) . Map.toList . U.hammer (Uc.convolve Floor kernel)
+  where
+    rule Empty 0 = Occupied
+    rule Occupied x = if x >= 4 then Empty else Occupied
+    rule s _ = s
+    kernel get = rule (get 0 0) (U.count (== Occupied) [ get (-1) (-1)
+                                                         , get (-1) 0
+                                                         , get (-1) 1
+                                                         , get 0 (-1)
+                                                         , get 0 1
+                                                         , get 1 (-1)
+                                                         , get 1 0
+                                                         , get 1 1
+                                                         ])
+
+
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB m = U.count ((== Occupied) . snd) . Map.toList . U.hammer update $ m
+  where
+    box = U.mapBoundingBox m
+    rule Floor _ = Floor
+    rule Empty 0 = Occupied
+    rule Empty _ = Empty
+    rule Occupied x = if x >= 5 then Empty else Occupied
+    visible m cx cy dx dy = fromMaybe Floor . List.find (/= Floor) . map (get m) . List.takeWhile (Uc.inBoundingBox box) . tail $ Uc.ray cx cy dx dy
+    get m key = Map.findWithDefault Floor key m
+    stencil m c@(cx, cy) = rule (get m c) (U.count (== Occupied) [ nearest (-1) (-1)
+                                                         , nearest (-1) 0
+                                                         , nearest (-1) 1
+                                                         , nearest 0 (-1)
+                                                         , nearest 0 1
+                                                         , nearest 1 (-1)
+                                                         , nearest 1 0
+                                                         , nearest 1 1
+                                                         ])
+      where nearest = visible m cx cy
+    update m = List.foldr (\key@(cx, cy) m' -> Map.insert key (stencil m key) m') m $ Map.keys m
