5eea7991a7e22d0a708df1c975d30d974a2a3300
Jake Faulkner <jake@devbox.phoenix>
2024-03-06 11:38:29 +1300

    Solve Day 22

--- src/Days/Day22.hs
+++ src/Days/Day22.hs
@@ -10,10 +10,13 @@ import qualified Data.Set as Set
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
 import qualified Util.Util as U
-
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
 import Data.Void
+import Data.Sequence (Seq(..), ViewR(..), ViewL(..), (<|), (|>), (><))
+import qualified Data.Sequence as Seq
+import Data.Bifunctor
+import Data.Monoid (Sum(Sum, getSum))
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +24,45 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = (,) <$> player <*> (endOfLine >> endOfLine >> player)
+  where player = do
+          "Player "
+          decimal
+          ":"
+          endOfLine
+          decimal `sepBy` endOfLine
 
 ------------ TYPES ------------
-type Input = Void
+type Input = ([Int], [Int])
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = sum . zipWith (*) [1..] . reverse . uncurry play
+  where play [] y = y
+        play x [] = x
+        play (x:xs) (y:ys) = if x > y then play (xs ++ [x, y]) ys else play xs (ys ++ [y, x])
+
+data Outcome = Player1 (Seq Int) | Player2 (Seq Int) deriving (Show, Eq)
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = getSum . Seq.foldMapWithIndex (\i x -> Sum $ (i + 1) * x) . Seq.reverse . stack . play mempty . sequify
+  where
+    sequify :: ([Int], [Int]) -> (Seq Int, Seq Int)
+    sequify = bimap Seq.fromList Seq.fromList
+    stack (Player1 v) = v
+    stack (Player2 v) = v
+    play :: Set (Seq Int, Seq Int) -> (Seq Int, Seq Int) -> Outcome
+    play _ (x, Empty) = Player1 x
+    play _ (Empty, y) = Player2 y
+    play history d@(x :<| xs, y :<| ys)
+         | d `Set.member` history = Player1 (fst d)
+         | x <= Seq.length xs && y <= Seq.length ys = case play mempty (Seq.take x xs, Seq.take y ys) of
+              Player1 _ ->  play (Set.insert d history) (xs >< Seq.fromList [x, y], ys)
+              Player2 _ ->  play (Set.insert d history) (xs, ys >< Seq.fromList [y, x])
+         | x > y = play (Set.insert d history) (xs >< Seq.fromList [x, y], ys)
+         | otherwise = play (Set.insert d history) (xs, ys >< Seq.fromList [y, x])
