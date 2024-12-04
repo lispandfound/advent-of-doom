9cf3b5f52fa021313ae8bda78a5ed0fb096cb342
Jake Faulkner <jakefaulkn@gmail.com>
2024-12-04 21:05:05 +1300

    Day 04

--- src/Days/Day04.hs
+++ src/Days/Day04.hs
@@ -1,7 +1,7 @@
-module Days.Day04 (runDay) where
+module Days.Day04 where
 
 {- ORMOLU_DISABLE -}
-import Data.List as List
+
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import Data.Maybe
@@ -12,30 +12,51 @@ import qualified Data.Vector as Vec
 import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay, Day)
-import Data.Attoparsec.Text
+import Data.Attoparsec.Text (Parser, anyChar)
 import Data.Void
 import Data.Functor (($>))
 import Data.Bifunctor
+import Data.Graph.Inductive.Graph
+import Data.Graph.Inductive.PatriciaTree (Gr(..))
+import Util.Parsers
+import Util.Coordinates
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
+data Letter = X | M | A | S deriving (Show, Eq, Generic, Ord)
+
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = coordinateParser (("X" $> pure X) <|> ("M" $> pure M) <|> ("A" $> pure A) <|> ("S" $> pure S) <|> (anyChar $> Nothing)) 0
 
------------- TYPES ------------
-type Input = Void
+type Input = CoordinateMap Letter
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA g = length $ do
+    ((x, y), l) <- Map.toList g
+    guard (l == X)
+    (dx, dy) <- [(dx1, dy1) | dx1 <- [-1, 0, 1], dy1 <- [-1, 0, 1], (dx1, dy1) /= (0, 0)]
+    guard $ take 4 (rayTillNothing g x y dx dy) == [X, M, A, S]
+    return ()
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = Map.size . convolve xmas
+  where
+    xmas get = do
+        a <- get 0 0
+        guard $ a == A
+        tl <- get (-1) 1
+        br <- get 1 (-1)
+        guard $ sort [tl, br] == [M, S]
+        bl <- get (-1) (-1)
+        tr <- get 1 1
+        guard $ sort [bl, tr] == [M, S]
+        return ()
--- src/Util/Coordinates.hs
+++ src/Util/Coordinates.hs
@@ -1,10 +1,17 @@
 module Util.Coordinates where
-import Data.Map (Map)
-import Data.Map as Map
-import Data.List as List
+
+import Data.Graph.Inductive.Graph
+import Data.Graph.Inductive.PatriciaTree (Gr (..))
+import Data.Map qualified as Map
 
 type CoordinateMap a = Map (Int, Int) a
 
+cantor :: Int -> Int -> Int
+cantor a b = ((m + n) * (m + n + 1)) `div` 2 + n
+  where
+    neg k = if k < 0 then 2 * k + 1 else 2 * k
+    m = neg a
+    n = neg b
 
 -- Takes a nested list (to be thought of as a 2D structure).
 -- Returns a map from "co-ordinates" to the items in the list.
@@ -12,22 +19,59 @@ type CoordinateMap a = Map (Int, Int) a
 --     Input: [[a,b,c],[d,e]]
 --     Output: Map.fromList [((0,0),a), ((0,1),b), ((0,2),c), ((1,0),d), ((1,1),e)]
 mapFromNestedLists :: (Ord a) => [[a]] -> CoordinateMap a
-mapFromNestedLists = Map.fromList . attachCoords 0 0
+mapFromNestedLists = fromList . attachCoords 0 0
   where
     attachCoords _ _ [] = []
     attachCoords x _ ([] : ls) = attachCoords (x + 1) 0 ls
     attachCoords x y ((l : ls) : lss) = ((x, y), l) : attachCoords x (y + 1) (ls : lss)
 
 -- Execute a convolution function at every defined point of a CoordinateMap. The caller is passed a function get, which can retreive nearby cells
-convolve :: a -> ((Int -> Int -> a) -> a) -> CoordinateMap a -> CoordinateMap a
-convolve def f m = List.foldr (\key@(cx, cy) m' -> Map.insert key (f $ get cx cy) m') m $ Map.keys m
-  where get cx cy dx dy = Map.findWithDefault def (cx + dx, cy + dy) m
+convolve :: ((Int -> Int -> Maybe a) -> Maybe b) -> CoordinateMap a -> CoordinateMap b
+convolve f m =
+    foldr
+        ( \key@(cx, cy) m' -> case (f $ get cx cy) of
+            Just v -> Map.insert key v m'
+            Nothing -> m'
+        )
+        mempty
+        $ Map.keys m
+  where
+    get cx cy dx dy = Map.lookup (cx + dx, cy + dy) m
 
--- Retreive all coordinates in a given direction
+-- Retrieve all coordinates in a given direction
 ray :: Int -> Int -> Int -> Int -> [(Int, Int)]
-ray cx cy dx dy = [(cx + i * dx, cy + i * dy) | i <- [0..]]
+ray cx cy dx dy = [(cx + i * dx, cy + i * dy) | i <- [0 ..]]
+
+rayTillNothing :: CoordinateMap a -> Int -> Int -> Int -> Int -> [a]
+rayTillNothing m cx cy dx dy = catMaybes . takeWhile isJust . map (`Map.lookup` m) $ ray cx cy dx dy
 
 type BoundingBox = (Int, Int, Int, Int)
 
 inBoundingBox :: BoundingBox -> (Int, Int) -> Bool
 inBoundingBox (lx, ux, ly, uy) (x, y) = lx <= x && x <= ux && ly <= y && y <= uy
+
+mapToFollowGraph :: CoordinateMap a -> Gr a ()
+mapToFollowGraph = mapToFollowGraphOn (\_ _ -> True)
+
+mapToFollowGraphOn :: (a -> a -> Bool) -> CoordinateMap a -> Gr a ()
+mapToFollowGraphOn follows = liftA2 mkGraph nodes edges
+  where
+    nodes = map (first (uncurry cantor)) . Map.toList
+    edges m =
+        [ (cantor x1 y1, cantor (x1 + dx) (y1 + dy), ())
+        | (x1, y1) <- Map.keys m
+        , dx <- [-1, 0, 1]
+        , dy <- [-1, 0, 1]
+        , (dx, dy) /= (0, 0) && fromMaybe False (follows <$> Map.lookup (x1, y1) m <*> Map.lookup (x1 + dx, y1 + dy) m)
+        ]
+
+terminalPaths :: [Node] -> Gr a b -> [Path]
+terminalPaths nodes graph = concatMap go nodes
+  where
+    go :: Node -> [Path]
+    go from
+        | outdeg graph from == 0 = [[from]]
+        | otherwise = do
+            next <- suc graph from
+            path <- go next
+            return (from : path)
