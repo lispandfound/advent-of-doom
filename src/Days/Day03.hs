79d0d943984de912079bc34ecc9ecd81e3189851
Jake Faulkner <jakefaulkn@gmail.com>
2024-12-04 16:06:13 +1300

    Day 03

--- src/Days/Day03.hs
+++ src/Days/Day03.hs
@@ -1,7 +1,8 @@
-module Days.Day03 (runDay) where
+{-# LANGUAGE TemplateHaskell #-}
+
+module Days.Day03 (runDay, Function (..), name, arguments) where
 
 {- ORMOLU_DISABLE -}
-import Data.List as List
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import Data.Maybe
@@ -10,32 +11,59 @@ import qualified Data.Set as Set
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
 import qualified Util.Util as U
+import Util.Util ((<<))
+import Util.Parsers (between)
 
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
 import Data.Void
 import Data.Functor (($>))
 import Data.Bifunctor
+import Replace.Attoparsec.Text
+import Data.Ix (inRange)
+import Control.Lens
+import Data.Text (isSuffixOf)
+import Data.Char (isAlpha)
 {- ORMOLU_ENABLE -}
+
+data Function = Function
+  { _name :: Text,
+    _arguments :: [Int]
+  }
+  deriving (Show, Generic)
+
+$(makeLenses ''Function)
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = toListOf (traversed . _Right) . splitCap function <$> takeText
+  where
+    function = Function <$> takeTill (\c -> c /= '_' && c /= '\'' && not (isAlpha c)) <*> between "(" ")" (decimal `sepBy` ",")
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Function]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = getSum . foldOf (traversed . isMul . correctArity . argsInRange . arguments . to (Sum . product))
+  where
+    isMul = filtered (("mul" `isSuffixOf`) . _name)
+    correctArity = filtered ((== 2) . length . _arguments)
+    argsInRange = filtered (all (inRange (0, 999)) . _arguments)
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = fst . foldl' go (0, True)
+  where
+    go (total, doit) (Function fn args)
+      | "mul" `isSuffixOf` fn && all (inRange (0, 999)) args && length args == 2 && doit = (total + product args, doit)
+      | "do" `isSuffixOf` fn = (total, True)
+      | "don't" `isSuffixOf` fn = (total, False)
+      | otherwise = (total, doit)
